b764ca5919e27967cb2838700a1604b39c68b0a4
Maximilian Konowski <m.konowski@itemsnet.de>
2022-10-16 14:56:48 +0200

    created accounting_transaction create mutation

--- app/graphql/mutations/accounting_transaction_create.rb
+++ app/graphql/mutations/accounting_transaction_create.rb
@@ -2,17 +2,27 @@
 
 module Mutations
   class AccountingTransactionCreate < BaseMutation
-    description "Creates a new accounting_transaction"
-
-    field :accounting_transaction, Types::AccountingTransactionType, null: false
+    description "Creates a new accounting transaction and return the new account_balance"
 
     argument :accounting_transaction_input, Types::AccountingTransactionInputType, required: true
 
-    def resolve(accounting_transaction_input:)
-      accounting_transaction = ::AccountingTransaction.new(**accounting_transaction_input)
-      raise GraphQL::ExecutionError.new "Error creating accounting_transaction", extensions: accounting_transaction.errors.to_hash unless accounting_transaction.save
+    field :success, Boolean, null: false
+    field :errors, [String], null: false
+    field :balance, Types::BankAccountTransactionType, null: true,
+      description: "The balance after this transaction"
 
-      { accounting_transaction: accounting_transaction }
+    def resolve(accounting_transaction_input:)
+      acc_tra = AccountingTransaction.new(
+        **accounting_transaction_input,
+        credit_account_id: context[:current_user].id
+      )
+      if acc_tra.save
+        acccount_balance = acc_tra.account_balances.find_sole_by(user: context[:current_user])
+        { success: true, balance: acccount_balance, errors: [] }
+      else
+        { success: false, balance: nil, errors: acc_tra.errors.full_messages }
+      end
     end
   end
 end
+
---
+++ app/graphql/types/accounting_transaction_input_type.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Types
+  class AccountingTransactionInputType < Types::BaseInputObject
+    argument :debit_account_id, Integer, required: true
+    argument :date, GraphQL::Types::ISO8601DateTime, required: true
+    argument :amount, Float, required: true
+  end
+end
